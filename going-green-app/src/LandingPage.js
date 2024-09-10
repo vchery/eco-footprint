ec43839d91331623afc735f4a52a8fda7e6e2ce2
valerie chery <87587798+vchery@users.noreply.github.com>
2024-09-09 22:35:04 -0400

    added clickable button to landing page

--- going-green-app/src/LandingPage.js
+++ going-green-app/src/LandingPage.js
@@ -1,17 +1,30 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { IconButton } from '@mui/material';
+import Fingerprint from '@mui/icons-material/Fingerprint';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-size: 1300px;
+  background-color: #EEEEEE;
+`;
 
 const LandingPage = () => {
     return (
         <Container>
-        
-            <p>Welcome!</p>
+            <div style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+                
+                <IconButton aria-label="fingerprint" color="success" component={Link} to="/signin">
+                    <Fingerprint />
+                </IconButton>  
 
+            </div>
         </Container>
     )
-
 }
 
 export default LandingPage;
